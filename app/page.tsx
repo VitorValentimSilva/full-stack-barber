90c7d48460a8537ad224abec2462452cf865a902
Vitor Valentim <vitorvalentin840@gmail.com>
2025-10-20 19:59:51 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,40 @@
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
 import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
 export default function Home() {
-  return <Button>Button</Button>
+  return (
+    <div>
+      <Header />
+
+      <main className="p-5">
+        <section>
+          <div>
+            <h2 className="text-xl font-bold">Ola, Vitor</h2>
+
+            <p>Segunda-feira, 05 de agosto</p>
+          </div>
+
+          <div className="mt-6 flex items-center gap-2">
+            <Input placeholder="Buscar serviços ou barbeiros" />
+
+            <Button>
+              <SearchIcon />
+            </Button>
+          </div>
+
+          <div className="relative mt-6 h-[150px] w-full">
+            <Image
+              src="/banner-01.png"
+              alt="Agente nos melhores com FWS Barber"
+              fill
+              className="w-full rounded-xl object-cover"
+            />
+          </div>
+        </section>
+      </main>
+    </div>
+  )
 }
